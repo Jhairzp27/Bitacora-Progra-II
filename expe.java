04c29c9e29ddd3caf6b8d9ceb8528111f394b24f
Jhairzp_27 <jhairzp27@hotmail.com>
2023-11-12 17:03:56 -0500

    C3: ad .gitignore

---
+++ expe.java
@@ -0,0 +1,11 @@
+public class expe {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        int variable3, total;
+        variable3 = 54;
+        total = variable3 + 2;
+        System.out.println(total);
+        System.out.println("HOLA A TODOOS BIENVENIDOS AL PROGRAMA ");
+        
+    }
+}
